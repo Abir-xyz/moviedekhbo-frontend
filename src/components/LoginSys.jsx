ff690d0eabd02a6926ec332b1b2907810e9151ea
Abir <samiulabir41@gmail.com>
2025-04-15 14:12:00 +0600

    changed whole navbar styles

--- src/components/LoginSys.jsx
+++ src/components/LoginSys.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { RiAccountCircleLine } from 'react-icons/ri';
+import styled from 'styled-components';
 
 const LoginSys = () => {
   const { loginWithRedirect, logout, user, isAuthenticated, isLoading } =
@@ -8,18 +10,43 @@ const LoginSys = () => {
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <div>
-      {!isAuthenticated ? (
-        <button onClick={() => loginWithRedirect()}>Log In</button>
-      ) : (
-        <>
-          <button onClick={() => logout({ returnTo: window.location.origin })}>
-            Log Out
+    <Wrapper className='section'>
+      <div>
+        {!isAuthenticated ? (
+          <button onClick={() => loginWithRedirect()}>
+            <span className='icon'>
+              <RiAccountCircleLine />
+            </span>{' '}
+            Log In
           </button>
-        </>
-      )}
-    </div>
+        ) : (
+          <>
+            <button
+              onClick={() => logout({ returnTo: window.location.origin })}
+            >
+              <span className='icon'>
+                <RiAccountCircleLine />
+              </span>
+              Log Out
+            </button>
+          </>
+        )}
+      </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.section`
+  button {
+    display: flex;
+    align-items: center;
+  }
+
+  .icon {
+    font-size: 1.4rem;
+    margin-right: 3px;
+    margin-top: 3px;
+  }
+`;
 
 export default LoginSys;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,48 @@
 import styled from 'styled-components';
 import { Link, NavLink } from 'react-router-dom';
 import { CiSearch } from 'react-icons/ci';
+import { CiHome } from 'react-icons/ci';
+import { MdMovie } from 'react-icons/md';
+import { MdOutlineLiveTv } from 'react-icons/md';
+import { FaFolderOpen } from 'react-icons/fa6';
+import { FaBarsStaggered } from 'react-icons/fa6';
 import LoginSys from './LoginSys';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
   const { isAuthenticated, user } = useAuth0();
+  const [isNav, setIsNav] = useState();
+  const [animate, setAnimate] = useState(false);
+
+  const handleShowLinks = () => {
+    setIsNav((prev) => !prev);
+    setAnimate(true);
+  };
+
+  useEffect(() => {
+    if (isNav) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [isNav]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setIsNav(false);
+        setAnimate(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <Wrapper className='section'>
@@ -15,19 +52,35 @@ const Navbar = () => {
             {isAuthenticated ? <h1>Hi, {user.name} </h1> : <h1>MovieDekhbo</h1>}
           </Link>
         </div>
-        <div className='wrapper'>
-          <div className='links'>
-            <NavLink to='/' className='link active-link'>
-              Home
+        <div className='wrapper link-wrapper'>
+          <div
+            className={`links ${isNav ? 'show-links' : ''} ${
+              animate ? 'animate-slide' : ''
+            }`}
+          >
+            <NavLink to='/' className='link active'>
+              <span className='icon'>
+                <CiHome />
+              </span>
+              <span>Home</span>
             </NavLink>
-            <NavLink to='/' className='link'>
-              Movies
+            <NavLink to='/movies' className='link'>
+              <span className='icon'>
+                <MdMovie />
+              </span>
+              <span>Movies</span>
             </NavLink>
-            <NavLink to='/' className='link'>
-              Series
+            <NavLink to='/tv' className='link'>
+              <span className='icon'>
+                <MdOutlineLiveTv />
+              </span>
+              <span>Series</span>
             </NavLink>
-            <NavLink to='/' className='link'>
-              WatchList
+            <NavLink to='/watchlist' className='link'>
+              <span className='icon'>
+                <FaFolderOpen />
+              </span>
+              <span>WatchList</span>
             </NavLink>
           </div>
         </div>
@@ -35,11 +88,25 @@ const Navbar = () => {
           <div className='global-btns'>
             <div className='search-wrapper'>
               <div className='search-btn'>
-                <CiSearch />
+                <span className='icon'>
+                  <CiSearch />
+                </span>
+                Search
               </div>
             </div>
+            <div className='nav-bars' onClick={handleShowLinks}>
+              <span>
+                <FaBarsStaggered />
+              </span>
+            </div>
+
             <div className='acc-wrapper'>
-              <div className='acc-btn'>
+              <div
+                className={`acc-btn ${isNav ? 'show-links' : ''} ${
+                  animate ? 'animate-slide' : ''
+                } `}
+                onClick={handleShowLinks}
+              >
                 <LoginSys />
               </div>
             </div>
@@ -66,26 +133,45 @@ const Wrapper = styled.section`
     color: var(--headings);
   }
   .links {
-    padding: 10px 0;
+    padding: 5px 0;
     border-radius: 10rem;
-    background: rgba(255, 255, 255, 0.1);
+    background: rgba(85, 85, 85, 0.2);
     box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(5.9px);
-    -webkit-backdrop-filter: blur(5.9px);
-    border: 1px solid rgba(39, 39, 39, 0.116);
+    backdrop-filter: blur(5px);
+    -webkit-backdrop-filter: blur(5px);
+    border: 1px solid rgba(0, 0, 0, 0.3);
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
   .link {
     text-decoration: none;
     color: #ffffffce;
-    font-size: 1.1rem;
+    font-size: 1rem;
     margin: 0 1rem;
     transition: all 0.1s ease-in-out;
+    display: flex;
+    flex-direction: row !important;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 2px;
+    text-align: center;
+    .icon {
+      font-size: 1.1rem;
+      margin-top: 5px;
+    }
   }
 
   .link:hover {
-    background-color: #222;
-    color: #fff;
-    padding: 6px 20px;
+    background-color: #9092ff;
+    padding: 6px 12px;
+    border-radius: 10rem;
+  }
+
+  .active {
+    background-color: #9092ff;
+    padding: 6px 12px;
     border-radius: 10rem;
   }
 
@@ -93,42 +179,46 @@ const Wrapper = styled.section`
     margin-right: 1rem;
   }
   .search-btn {
-    color: #000000;
-    height: 40px;
-    width: 40px;
-    border-radius: 50%;
-    position: relative;
+    color: #fff;
+    font-size: 1rem;
+    .icon {
+      font-size: 1.4rem;
+      margin-top: 5px;
+      margin-right: 3px;
+    }
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid #fff;
+    padding: 0px 15px;
+    border-radius: 10rem;
+    background: rgba(85, 85, 85, 0.2);
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.185);
+    backdrop-filter: blur(5px);
+    -webkit-backdrop-filter: blur(5px);
+    border: 1px solid rgba(0, 0, 0, 0.3);
     cursor: pointer;
-    background-color: #ffffff;
     transition: all 0.1s ease-in-out;
-    border: 1px solid #ffff;
   }
   .search-btn:hover {
-    background-color: #c0c0c0 !important;
-    border-color: #c0c0c0 !important;
-    color: #fff !important;
+    box-shadow: rgba(255, 255, 255, 0.062) 0px 2px 8px;
   }
-  .search-btn svg {
-    font-size: 1.8rem;
-    position: absolute;
-    top: 11%;
-    left: 13%;
-  }
+
   .acc-btn button {
-    font-size: 1.1rem;
-    color: #000000 !important ;
-    padding: 10px 26px;
-    cursor: pointer;
+    font-size: 1rem;
+    color: #fff;
+    padding: 4px 15px;
     border-radius: 10rem;
-    border: 1px solid #fff;
-    background-color: #fff;
+    border: none;
+    background-color: #7678ed;
+    cursor: pointer;
     transition: all 0.1s ease-in-out;
+    box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
+      rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
   }
 
   .acc-btn button:hover {
-    background-color: #c0c0c0 !important;
-    color: #fff !important;
-    border-color: #c0c0c0 !important;
+    background-color: #9092ff;
   }
 
   .global-btns {
@@ -136,17 +226,46 @@ const Wrapper = styled.section`
     align-items: center;
   }
 
-  .active-link {
-    background-color: #222;
+  .nav-bars {
+    display: none;
     color: #fff;
-    padding: 6px 20px;
-    border-radius: 10rem;
+    font-size: 1.3rem;
+    cursor: pointer;
   }
 
-  @media screen and (max-width: 800px) {
+  @media screen and (max-width: 1000px) {
     .brand h1 {
-      font-size: 1.66rem;
+      font-size: 1.3rem;
     }
+    .link {
+      font-size: 0.9rem;
+      .icon {
+        font-size: 1rem;
+        margin-top: 3px;
+      }
+    }
+    .active-link {
+      padding: 2px 8px;
+    }
+    .link:hover {
+      padding: 2px 8px;
+    }
+    .search-btn {
+      font-size: 0.9rem;
+      .icon {
+        font-size: 1.2rem;
+      }
+    }
+    .acc-btn button {
+      font-size: 0.9rem;
+      padding: 2px 8px;
+      .icon {
+        font-size: 1.2rem;
+      }
+    }
+  }
+
+  @media screen and (max-width: 900px) {
     .links {
       position: fixed;
       z-index: 10;
@@ -161,38 +280,18 @@ const Wrapper = styled.section`
       margin-left: auto;
       margin-right: auto;
       padding: 6px 16px;
-      /* From https://css.glass */
-      background: rgba(70, 70, 70, 0.2);
+      background: rgba(85, 85, 85, 0.2);
       box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-      backdrop-filter: blur(5.9px);
-      -webkit-backdrop-filter: blur(5.9px);
-      border: 1px solid rgba(206, 206, 206, 0.21);
+      backdrop-filter: blur(5px);
+      -webkit-backdrop-filter: blur(5px);
+      border: 1px solid rgba(0, 0, 0, 0.3);
     }
     .link {
-      font-size: 1rem;
       margin: 0 !important;
       color: #ffffffce;
     }
     .active-link {
       color: var(--txtClr) !important;
-    }
-
-    .search-btn {
-      background-color: var(--primaryClr);
-      color: #000 !important;
-      height: 30px;
-      width: 30px;
-      border-radius: 50%;
-      position: relative;
-    }
-    .search-btn svg {
-      font-size: 1.3rem;
-      position: absolute;
-      top: 11%;
-      left: 13%;
-    }
-    .acc-btn button {
-      padding: 4px 8px;
     }
 
     .navbar-container {
@@ -203,12 +302,95 @@ const Wrapper = styled.section`
     }
   }
 
-  @media screen and (min-width: 800px) {
+  @media screen and (max-width: 768px) {
+    .links {
+      position: absolute;
+      top: 15%;
+      left: 0%;
+      right: 0;
+      width: 100%;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: flex-start;
+      min-width: 100%;
+      border-radius: 0;
+      min-height: 100vh;
+      z-index: 10001 !important;
+      /* display: none; */
+      background-color: #000814;
+      /* padding-top: 3.66rem; */
+      padding: 3.66rem 1.5rem; /* add horizontal padding */
+      border-top-right-radius: 26px !important;
+      border-top-left-radius: 26px !important;
+      box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+        rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+      transform: translateX(-100vw);
+      transition: none;
+      .link {
+        margin: 0.8rem 0 !important;
+        font-size: 1.1rem !important;
+        width: 100%;
+        padding: 8px 12px;
+        display: flex;
+        justify-content: flex-start !important;
+      }
+      .icon {
+        font-size: 1.3rem;
+        margin-right: 5px;
+      }
+    }
+    .show-links {
+      display: flex !important;
+      transform: translateX(0) !important;
+    }
+    .animate-slide {
+      transition: transform 0.3s ease-in-out;
+    }
+    .acc-wrapper {
+      position: absolute;
+      /* top: 0%; */
+      left: 0;
+      right: 0%;
+      bottom: 5%;
+      z-index: 10002;
+      width: 100%;
+      padding-top: 2.2rem;
+      width: 90%;
+      margin: 0 auto;
+      .acc-btn {
+        width: 100%;
+        /* display: none; */
+        transform: translateX(-100vw);
+      }
+      .acc-btn section {
+        width: 100% !important;
+      }
+      .acc-btn button {
+        font-size: 1.1rem;
+        width: 100%;
+        display: flex !important;
+        align-items: center !important;
+        justify-content: center !important;
+      }
+      .acc-btn svg {
+        font-size: 1.4rem;
+      }
+    }
+    .nav-bars {
+      display: block;
+      margin-top: 7px;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
     .navbar-container {
       display: flex;
       align-items: center;
       justify-content: space-between;
       padding-top: 40px;
+    }
+    .links {
+      display: flex !important;
     }
   }
 `;
